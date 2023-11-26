769a57321191ba23f3b0004f427452723ebd22a9
mmdreza <mmdvirus80@gmail.com>
2023-11-26 19:07:36 +0330

    add Mongodb Schema

---
+++ models/users.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1/Friends")
+    .then(() => console.log("MongoDB Connected ..."))
+    .catch((err) => console.log(err.message))
+
+const userSchema = mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    age: { type: Number, required: true },
+    job: { type: String, required: true },
+    isCar: { type: String, default: false },
+})
+
+const User = mongoose.model("Users", userSchema)
+
+
+module.exports = {
+    User
+}
